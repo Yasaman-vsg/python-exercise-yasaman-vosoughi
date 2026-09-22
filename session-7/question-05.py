997be7d6640bb58f3a227d8e62c2a85b0ba50eed
Yasaman-vsg <vosoughiyasamn@gmail.com>
2026-09-22 12:07:25 -0700

    add session 7 exercis

--- session-7/question-05.py
+++ session-7/question-05.py
@@ -15,28 +15,32 @@ logs = [
     ("Sara", "LOGIN", 403),
     ("Sara", "LOGIN", 403),
 ]
-
-log_count=0
-users={}
-mashkok=[]
-for log in logs:
-    name=log[0]
-    action=log[1]
-    status=log[2]
-    if action=='Login' and status==200:
-        users[name]['success']+=1      
-      
-    elif action=='Login' and status==403:
-        users[name]['failed']+=1
-    if name not in users:
-        users[name]={'success':0,
-                     'failed':0,
-                     'operation':0}
-    users[name]['operation']+=1    
-    
-        
-    if users[name]['failed']>3:
-        mashkok.append(name)
+def analyze_logs(logs):
+    log_count=0
+    users={}
+    mashkok=[]
+    for log in logs:
+        name=log[0]
+        action=log[1]
+        status=log[2]
+       
+        if name not in users:
+            users[name]={'success':0,
+                         'failed':0,
+                         'operation':0}
+        users[name]['operation']+=1    
+        if action=='LOGIN' and status==200:
+            users[name]['success']+=1      
+          
+        elif action=='LOGIN' and status==403:
+            users[name]['failed']+=1
+            
+        if users[name]['failed']>=3:
+            mashkok.append(name)
+    return users,mashkok
+users,mashkok=analyze_logs(logs) 
 for name in users:
     print(name,':',users[name])        
-print('suspicious users:',mashkok)       
+print('suspicious users:',mashkok)  
+
+     
